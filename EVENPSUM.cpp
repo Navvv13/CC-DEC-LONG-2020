f158caf3bd14fc2b0aa2b243dc5e8ed3c9462a6a
Navneet Kaur Assi <65205930+Navvv13@users.noreply.github.com>
2020-12-13 03:05:10 +0530

    Create EVENPSUM.cpp

---
+++ EVENPSUM.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	long long int t;
+	cin>>t;
+	while(t--)
+	{
+		long long int a, b;
+		cin>>a>>b;
+		if(a*b%2==0)
+		{
+		   cout<<(a*b)/2<<endl;
+		}
+		else
+		{
+		    cout<<(a*b)/2+1<<endl;
+		}
+	
+	}
+	return 0;
+}
